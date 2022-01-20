f7734b831c35e1dc05de292ad496154453551650
Sid <sepner@vmware.com>
2022-01-20 09:10:01 -0500

    added clientapiid for partner usecase

--- update-accounts-lambda.py
+++ update-accounts-lambda.py
@@ -1,16 +1,43 @@
+# This function works by calling into CH and looking for unconfigured accounts and updating their configuration with a single common role name.
+# this function is not aware of multiple organization structures and will attempt to configure any unconfigured account in your CH environment.
+# for example: if you have multiple aws organizations, you will need to ensure all CH access roles in all linked accounts across all organizations use a common name.
+
 import os
 import http.client
 import json
 
 api_key = os.environ.get('api_key')
 external_id = os.environ.get('external_id')
-friendly_name_prefix = os.environ.get('friendly_name_prefix')
 arn_role_name = "CloudHealth-Access"
+client_api_id = os.environ.get('client_api_id')
 
 def lambda_handler(event, context):
-    def get_unconfigured_accounts(api_key):
+    def customer_check(client_api_id, api_key):
+        if client_api_id == "Not a partner":
+            return False
+        else:
+            base_url = 'chapi.cloudhealthtech.com'
+            query = '/v1/customers/%s' % client_api_id
+            headers = {'Content-type': 'application/json', 'Authorization': 'Bearer %s' % api_key}
+            try:
+                connection = http.client.HTTPSConnection(base_url)
+                connection.request('GET', query, headers = headers)
+                status = connection.getresponse().status
+                connection.close()
+                if status == 200:
+                    return True
+                else:
+                    print('\n',"--------------------------", '\n', "Please check the client ID provided is correct, or that the customer account is active. \n")
+                    return False
+            except http.client.InvalidURL:
+                print('\n',"--------------------------", '\n', "The client ID provided is not valid\n")
+                return False
+    
+    def get_unconfigured_accounts(api_key, client_api_id = 0):
         base_url = 'chapi.cloudhealthtech.com'
-        query = '/api/search.json?api_version=2&name=AwsAccount&query=status=\'Not-Configured\'&fields=owner_id'
+        query = '/api/search.json?api_version=2&name=AwsAccount&query=status=\'Not-Configured\'&fields=owner_id,name'
+        if client_api_id:
+            query = query + '&client_api_id=%s' % client_api_id
         headers = {'Content-type': 'application/json', 'Authorization': 'Bearer %s' % api_key} 
         connection = http.client.HTTPSConnection(base_url)
         connection.request('GET', query, headers = headers)
@@ -18,11 +45,18 @@ def lambda_handler(event, context):
         connection.close()
         return response
     
-    unconfigured_accounts = get_unconfigured_accounts(api_key)
-
-    def update_account(api_key, ch_account_id, friendly_name, role_arn, external_id):
+    if customer_check(client_api_id, api_key) == False:
+        is_customer = False
+        unconfigured_accounts = get_unconfigured_accounts(api_key)
+    else:
+        is_customer = True
+        unconfigured_accounts = get_unconfigured_accounts(api_key, client_api_id)
+    
+    def update_account(api_key, ch_account_id, friendly_name, role_arn, external_id, client_api_id = 0):
         base_url = 'chapi.cloudhealthtech.com'
         query = '/v1/aws_accounts/%s' % ch_account_id
+        if client_api_id:
+            query = query + '?client_api_id=%s' % client_api_id
         headers = {'Content-type': 'application/json', 'Authorization': 'Bearer %s' % api_key} 
         account_info = {
             "name": "%s" % friendly_name, 
@@ -40,9 +74,15 @@ def lambda_handler(event, context):
         connection.close()
         return response
     
+    print('\n',"--------------------------", '\n')
+    
     for item in unconfigured_accounts:
         role_arn = 'arn:aws:iam::%s:role/%s' % (item['owner_id'], arn_role_name)
-        friendly_name = friendly_name_prefix + " - " + str(item['owner_id'])
-        print('\n',"--------------------------", '\n', 'Owner ID: ', item['owner_id'], ', CH Account ID: ', item['id'], ', Friendly Name: ', friendly_name)
-        update_account(api_key, item['id'], friendly_name, role_arn, external_id)
+        friendly_name = item['name']
+        print('Owner ID: ', item['owner_id'], ', CH Account ID: ', item['id'], ', Friendly Name: ', friendly_name)
+        if is_customer == True:
+            update_account(api_key, item['id'], friendly_name, role_arn, external_id, client_api_id)
+        else:
+            update_account(api_key, item['id'], friendly_name, role_arn, external_id)
+    print('\n',"--------------------------", '\n')
     return event
